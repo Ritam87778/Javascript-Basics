4c4de015f4744c9999a09ccecf42783da7d073cb
Ritam <ritamdutta87778@gmail.com>
2025-01-22 14:37:55 +0530

    conversion in javascript

---
+++ 02index.js
@@ -0,0 +1,27 @@
+// Conversion in javascript
+
+// string to number
+
+let num = "100";
+
+console.log(num);
+
+console.log(typeof num);
+
+const valueInNumber = Number(num);
+
+console.log(valueInNumber);
+
+const num2 = "331a";
+
+let valueInNumber2 = Number(num2);
+
+console.log(valueInNumber2);
+
+const bool = true;
+
+const boolvalue = Number(bool);
+
+console.log(typeof valueInNumber2);
+
+console.log(boolvalue);
