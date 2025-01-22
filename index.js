404e16901bb270d0d288d48cb6fcd3edd0f829c9
Ritam <ritamdutta87778@gmail.com>
2025-01-22 13:22:10 +0530

    data types in javascript

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-// Data Types
+// Data Types & Variables
 
-var name = "Rahul" ;
-console.log(name)   ;    
+var name = "John Doe";
+console.log(name);
+
+const age = 27;
+console.log(age);
+
+let email = "iwritecode000@gmail.com";
+
+email = "ritamdutta87778@gmail.com";
+
+console.log(email);
+
+let state;
+
+/*
+prefer not to use var
+because of issue in block scope & functional scope
+use const for values that should not change
+*/
+
+// Types of Data
+
+// string
+// number
+// boolean
+// null
+// undefined
+
+const message = "Hello World";
+
+console.log(typeof message);
+
+console.log(typeof age);
+
+console.log(typeof state);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
